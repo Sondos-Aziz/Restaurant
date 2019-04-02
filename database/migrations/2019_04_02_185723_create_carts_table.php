3da7f680ef5d391bc85652da85ce50d15d4d53d9
Sondos <sondos1751998@gmail.com>
2019-04-02 22:48:51 +0300

    create_carts_table

---
+++ database/migrations/2019_04_02_185723_create_carts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->bigInteger('customer_id')->nullable($value = false)->unsigned();
+            $table->bigInteger('item_id')->nullable($value = false)->unsigned();
+            $table->integer('quantity');
+            $table->primary(['id', 'item_id']);
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
